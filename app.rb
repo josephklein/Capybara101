d1d90adbe2c9d4d496ccd189d325a3a21320f3cb
Jay Bobo <jbobo@covermymeds.com>
2014-12-11 13:27:07 -0500

    lightweight app to use for testing

--- app.rb
+++ app.rb
@@ -1,7 +1,34 @@
 require 'sinatra'
 require 'sass'
 
+enable :sessions
+
+helpers do
+  def logged_in?
+    session[:user]
+  end
+end
+
 get "/" do
   haml :index
 end
 
+post '/login' do
+  if params[:username] == params[:password]
+    session[:user] = params[:username]
+    redirect to '/dashboard'
+  else
+    redirect back
+  end
+end
+
+get '/logout' do
+  @message = 'You have been logged out'
+  session.clear
+  haml :index
+end
+
+get "/dashboard" do
+  @username = session[:user]
+  haml :dashboard
+end
