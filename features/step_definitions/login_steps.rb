acf9647ce73a7cdcd141ff95531056756c38e788
Nicolas McCurdy <thenickperson@gmail.com>
2015-01-15 21:27:33 -0500

    Use Strings instead of Regexps for step definitions

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,13 +1,13 @@
-Given /^the login page$/ do
+Given 'the login page' do
   visit '/'
 end
 
-When /^I log in with proper credentials$/ do
+When 'I log in with proper credentials' do
   fill_in 'username', with: 'example'
   fill_in 'password', with: 'example'
   click_button 'Login'
 end
 
-Then /^I should see the secret page$/ do
+Then 'I should see the secret page' do
   expect(page).to have_content 'This is the secret page.'
 end
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,13 +1,13 @@
-And /^I log out$/ do
+And 'I log out' do
   click_link 'Logout'
 end
 
-Then /^I should see the login page$/ do
+Then 'I should see the login page' do
   expect(page).to have_content 'This is login page for users.'
 end
 
 # This will fail because the secret page is not properly hidden.
-And /^I should not be able to visit the secret page$/ do
+And 'I should not be able to visit the secret page' do
   visit '/dashboard'
   expect(page.status_code).to be_between(400, 499)
 end
