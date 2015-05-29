7651370964499981dd533f98a608fd7deb2cbb97
Peter Mueller <pmueller@covermymeds.com>
2015-05-29 15:52:36 -0400

    add capy/cuke tests for logout

--- features/logout.feature
+++ features/logout.feature
@@ -1,1 +1,10 @@
-#Add your codez here
+Feature: Logout Page
+  In order to allow users to protect themselves
+  As a user
+  I want to be able logout of my account
+
+  Scenario: Logged-in User
+    Given I am logged in
+    When I visit the dashboard
+    And I select logout
+    Then I should see the homepage with a logout message
--- features/step_definitions/logout_steps.rb
+++ features/step_definitions/logout_steps.rb
@@ -1,1 +1,18 @@
-#Add your codez here
+Given(/^I am logged in$/) do
+  visit '/'
+  fill_in 'username', with: 'a_username'
+  fill_in 'password', with: 'a_username'
+  click_button 'Login'
+end
+
+When(/^I visit the dashboard$/) do
+  visit '/dashboard'
+end
+
+When(/^I select logout$/) do
+  click_link 'Logout'
+end
+
+Then(/^I should see the homepage with a logout message$/) do
+  expect(page).to have_content 'You have been logged out'
+end
